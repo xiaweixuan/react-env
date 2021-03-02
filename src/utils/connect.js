63e9582cfb1fac033317dbb9fa776c574098d5fe
夏炜轩 <xiaweixuan@bytedance.com>
2021-03-02 12:16:40 +0800

    更改connect

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -1,20 +1,13 @@
 import React from 'react';
 
-// const connect = (...storeList) => {
-//   return (ComponentView) => (props) => {
-//     const list = [...storeList, ComponentView].reverse();
-//     let el = null;
-//     list.forEach((Item) => {
-//       el = <Item {...props}>{el}</Item>;
-//     });
-//     return el;
-//   };
-// }
-const connect = (Store) => {
-  const StoreCom = Store;
-  return (ComponentView) => {
-    return (props) => <StoreCom><ComponentView {...props} /></StoreCom>
-  }
+const connect = (...storeList) => {
+  return (ComponentView) => (props) => {
+    const list = [...storeList, ComponentView].reverse();
+    let el = null;
+    list.forEach((Item) => {
+      el = <Item {...props}>{el}</Item>;
+    });
+    return el;
+  };
 }
-
 export default connect;
